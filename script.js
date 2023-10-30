046dc67ca66812a38379d33a5a859f0c115ae683
MrPalashJ <palashofc@gmail.com>
2023-10-29 23:50:22 -0500

    final

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// /**********************************************************************
+// * _______            __                      __                 _____            __           
+// *|       \          |  \                    |  \               |     \          |  \          
+// *| $$$$$$$\ ______  | $$  ______    _______ | $$____            \$$$$$  ______   \$$ _______  
+// *| $$__/ $$|      \ | $$ |      \  /       \| $$    \             | $$ |      \ |  \|       \ 
+// *| $$    $$ \$$$$$$\| $$  \$$$$$$\|  $$$$$$$| $$$$$$$\       __   | $$  \$$$$$$\| $$| $$$$$$$\
+// *| $$$$$$$ /      $$| $$ /      $$ \$$    \ | $$  | $$      |  \  | $$ /      $$| $$| $$  | $$
+// *| $$     |  $$$$$$$| $$|  $$$$$$$ _\$$$$$$\| $$  | $$      | $$__| $$|  $$$$$$$| $$| $$  | $$
+// *| $$      \$$    $$| $$ \$$    $$|       $$| $$  | $$       \$$    $$ \$$    $$| $$| $$  | $$
+// * \$$       \$$$$$$$ \$$  \$$$$$$$ \$$$$$$$  \$$   \$$        \$$$$$$   \$$$$$$$ \$$ \$$   \$$
+// * 
+// * ITM-541-05
+// * Date: 08/28/2023
+// *
+// * Assignment: Lab 3
+// * 
+// * Description: Tip Calculator 
+// *
+// ***********************************************************************/
+// Get references to HTML elements
 const billTotal = document.getElementById("billTotal");
 const tipSlider = document.getElementById("tipSlider");
 const tipPercentage = document.getElementById("tipPercentage");
@@ -5,14 +25,16 @@ const tipAmount = document.getElementById("tipAmount");
 const totalBill = document.getElementById("totalBill");
 const errorMessage = document.getElementById("error-message");
 const split = document.getElementById("split");
-const amountPerPerson = document.getElementById("amountPerPerson"); 
+const amountPerPerson = document.getElementById("amountPerPerson");
 const resetButton = document.getElementById("resetButton");
 
+// Add event listeners
 billTotal.addEventListener("input", calculateTip);
 tipSlider.addEventListener("input", calculateTip);
 split.addEventListener("input", calculateTip);
 resetButton.addEventListener("click", resetForm);
 
+// Calculate and display tip
 function calculateTip() {
     const bill = parseFloat(billTotal.value);
     const tip = parseFloat(tipSlider.value);
@@ -28,10 +50,11 @@ function calculateTip() {
         amountPerPerson.value = perPerson.toFixed(2);
         errorMessage.textContent = "";
     } else {
-        errorMessage.textContent = "Please enter a valid number for Bill Total and ensure there is at least 1 person.";
+        errorMessage.textContent = "Please enter a valid number for Bill Total.";
     }
 }
 
+// Reset the form
 function resetForm() {
     billTotal.value = "";
     tipSlider.value = 0;
@@ -43,16 +66,17 @@ function resetForm() {
     amountPerPerson.value = "";
 }
 
+// Show or hide the "split" input field based on user choice
 function openBox(type) {
-    let splitElement = document.getElementById("splitB")
+    let splitElement = document.getElementById("splitB");
     switch (type) {
         case 'y':
-            splitElement.style.display = "block"
+            splitElement.style.display = "block";
             break;
         case 'n':
-            splitElement.style.display = "none"
+            splitElement.style.display = "none";
             split.value = 1;
-            this.calculateTip();
+            calculateTip(); // Recalculate the tip when hiding the "split" input
             break;
         default:
             break;
